2138d10f62af312e73d241745f48b44db8e48b70
Anton Hollube <anton.hollube@gmail.com>
2024-05-09 22:32:23 +0200

    Mergesort added

---
+++ SortAlgorithms/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+
+        System.out.println("Given array is");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array is");
+        printArray(arr);
+    }
+
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver code
+}
